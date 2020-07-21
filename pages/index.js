efb85305c1abe7e4566566cd8995a8c09d36ab80
madhuprya <priyamadhu536@gmail.com>
2020-07-21 11:56:54 +0530

    seo integration

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import Head from "next/head";
-import { connect } from "react-redux";
 import NewsHeader from "../src/Components/Header/NewsHeader";
 import NewsSection from "../src/Components/NewsSection/NewsSection";
 import TopNews from "../src/Components/TopNews/TopNews";
-import { LoadingOutlined } from "@ant-design/icons";
 import { Layout } from "antd";
-class HomePage extends Component {
-  static async getInitialProps({ store }) {
-    const serverData = store.getState((serverStoreData) => {
-      return serverStoreData;
-    });
+export default class HomePage extends Component {
+  static async getInitialProps({ req }) {
+    // const serverData = store.getState((serverStoreData) => {
+    //   return serverStoreData;
+    // });
+    let newsData = new Array();
+    if (req.params && req.params.source_name) {
+      let metaData = {
+        title: `news source  ${req.params.source_name} `,
+        description: `search by any news provider name like ${req.params.source_name} `,
+        keywords: `News Provided by ${req.params.source_name}`,
+        url: `localhost:3000/source/${req.params.source_name}`,
+      };
+      newsData["metaData"] = metaData;
+    }
+    return { ...newsData };
   }
 
   constructor(props) {
@@ -21,7 +30,14 @@ class HomePage extends Component {
     return (
       <div>
         <Head>
-          <title>News Home Page</title>
+          <title>
+            {this.props.metaData.title
+              ? this.props.metaData.title
+              : "News Home Page"}
+          </title>
+          <meta name="description" content={this.props.metaData.description} />
+          <meta name="keyword" content={this.props.metaData.keywords} />
+          <link rel="canonical" href={this.props.metaData.url} />
         </Head>
         <Layout>
           <NewsHeader />
@@ -32,9 +48,3 @@ class HomePage extends Component {
     );
   }
 }
-
-const mapStateToProps = (state) => ({
-  loading: state.newsDetail.loading,
-});
-
-export default connect(mapStateToProps)(HomePage);
